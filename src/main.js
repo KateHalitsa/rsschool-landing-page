eb21279ea78a957db4817f26f3cffbd7f019161c
Katya <kate.halitsa@gmail.com>
2026-09-20 20:41:00 +0300

    feat: add catalog

--- src/main.js
+++ src/main.js
@@ -23,3 +23,27 @@ import './styles/hero.scss'
 
 setupCounter(document.querySelector('#counter'))
 */
+const descriptions = document.querySelectorAll('.description');
+
+descriptions.forEach((description) => {
+    const button = description.querySelector('.description__button');
+
+    button.addEventListener('click', () => {
+        description.classList.toggle('description--expanded');
+
+        const isExpanded = description.classList.contains('description--expanded');
+
+        button.textContent = isExpanded ? 'Less':'More';
+    });
+});
+const options = document.querySelectorAll('.option');
+
+options.forEach((option) => {
+    option.addEventListener('click', () => {
+        options.forEach((item) => {
+            item.classList.remove('active');
+        });
+
+        option.classList.add('active');
+    });
+});
