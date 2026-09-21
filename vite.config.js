be87eccc9bbd05095dfc34ff5104d97d0f6a7a83
Katya <kate.halitsa@gmail.com>
2026-09-21 19:51:00 +0300

    fix: fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: './',
+
   build: {
     sourcemap: true,
     minify: false,
+
+    rollupOptions: {
+      input: {
+        main: resolve(import.meta.dirname, 'index.html'),
+        catalog: resolve(import.meta.dirname, 'catalog.html'),
+      },
+    },
   },
 });
